d46977b8bfc76ed1b42970f3167d605838b39f5e
Shraddha Muley <66071175+shraddha15-creator@users.noreply.github.com>
2022-06-01 11:52:28 +0530

    fix: private route

--- src/utilities/PrivateRoute.js
+++ src/utilities/PrivateRoute.js
@@ -1,11 +1,12 @@
 import React from "react";
 import { Navigate, useLocation } from "react-router-dom";
-import { useAuth } from "../context/auth-context";
+import { token } from "./token";
+
+const encodedToken = token();
 
 const PrivateRoute = ({ children }) => {
 	const location = useLocation();
-	const { user } = useAuth();
-	return user && user.token ? (
+	return encodedToken && encodedToken ? (
 		children
 	) : (
 		<Navigate to="/login" state={{ from: location?.pathname }} replace />
