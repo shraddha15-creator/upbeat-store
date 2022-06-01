9cb7b98a95371e923315c5844262ec8888cec424
Shraddha Muley <66071175+shraddha15-creator@users.noreply.github.com>
2022-06-01 20:04:22 +0530

    fix: netlify deploy error

--- src/pages/SingleProduct/SingleProduct.js
+++ src/pages/SingleProduct/SingleProduct.js
@@ -1,5 +1,4 @@
-import axios from "axios";
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { Link, useParams } from "react-router-dom";
 import { useProducts } from "../../context/product-context";
 import Page404 from "../404page/Page404";
