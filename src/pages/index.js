7e74765d51463255ef23e636090370f891c0d37b
Shraddha Muley <66071175+shraddha15-creator@users.noreply.github.com>
2022-06-02 11:43:58 +0530

    fix: refactor code

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,6 @@
-export { Home } from "./Home/Home";
 export { Page404 } from "./404page/Page404";
-export { Cart } from "./Cart/Cart";
-// export { Home } from "./Home/Home";
-// export { Products } from "./ProductListing/Products";
+export { Home } from "./Home/Home";
+export { Products } from "./ProductListing/Products";
 export { SingleProduct } from "./SingleProduct/SingleProduct";
 export { Wishlist } from "./Wishlist/Wishlist";
-export {} from ".";
-export {} from ".";
-export { Products } from "./ProductListing/Products";
+export { Cart } from "./Cart/Cart";
