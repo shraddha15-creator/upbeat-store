daaa7f7f57fbec7a5bfef0cf159b00aa94d91d94
Shraddha Muley <66071175+shraddha15-creator@users.noreply.github.com>
2022-06-08 10:17:33 +0530

    fix: categories on home page

--- src/backend/db/categories.js
+++ src/backend/db/categories.js
@@ -8,21 +8,21 @@ import { v4 as uuid } from "uuid";
 export const categories = [
 	{
 		_id: uuid(),
-		categoryName: "Acoustic",
+		categoryName: "acoustic",
 		img: "https://raw.githubusercontent.com/shraddha15-creator/e-commerce/dev/images/Acoustic-guitar.jpg",
 		description:
 			"literature in the form of prose, especially novels, that describes imaginary events and people",
 	},
 	{
 		_id: uuid(),
-		categoryName: "Electric",
+		categoryName: "electric",
 		img: "https://raw.githubusercontent.com/shraddha15-creator/e-commerce/dev/images/electric-guitar.jpg",
 		description:
 			"Non-fiction is writing that gives information or describes real events, rather than telling a story.",
 	},
 	{
 		_id: uuid(),
-		categoryName: "Hollowbody",
+		categoryName: "hollowbody",
 		img: "https://raw.githubusercontent.com/shraddha15-creator/e-commerce/dev/images/hollowbody-guitar.jpg",
 		description:
 			"Meant to cause discomfort and fear for both the character and readers, horror writers often make use of supernatural and paranormal elements in morbid stories that are sometimes a little too realistic.",
@@ -30,7 +30,7 @@ export const categories = [
 
 	{
 		_id: uuid(),
-		categoryName: "Classic",
+		categoryName: "classic",
 		img: "https://raw.githubusercontent.com/shraddha15-creator/e-commerce/dev/images/classic.jpg",
 		description:
 			"Meant to cause discomfort and fear for both the character and readers, horror writers often make use of supernatural and paranormal elements in morbid stories that are sometimes a little too realistic.",
